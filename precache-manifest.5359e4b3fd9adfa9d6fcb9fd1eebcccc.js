2472106e7f54173ba7ec2f0f4845f45998c29a78
mikiteek <mikiteek@gmail.com>
2020-09-25 11:45:13 +0300

    Updates

---
+++ precache-manifest.5359e4b3fd9adfa9d6fcb9fd1eebcccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414b05c5395c7a21e6a69c4df879c2df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f6169e6dd8010c45e9e",
+    "url": "./static/css/main.ebfa2173.chunk.css"
+  },
+  {
+    "revision": "34fdc677467734a760a5",
+    "url": "./static/js/2.cb29649f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cb29649f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6169e6dd8010c45e9e",
+    "url": "./static/js/main.5e742d5b.chunk.js"
+  },
+  {
+    "revision": "c59c8349261324fa9b9c",
+    "url": "./static/js/runtime-main.83ee23a1.js"
+  }
+]);
